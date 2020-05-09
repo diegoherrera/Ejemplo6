95d93116b76f63c78f0039c1d0a3131efe837b2c
Diego Sebastian Herrera <diherrer@everis.com>
2020-05-08 22:44:40 -0300

    first commit

---
+++ test.js
@@ -0,0 +1,8 @@
+var arregloa = [1, 2, 3, 4, 5, 6];
+var arreglob = [12, 33, 43, 54, 62];
+arregloa.forEach(function (element) {
+    console.log(element);
+});
+for (var i = 0; i < arreglob.length; i++) {
+    console.log(arreglob[i]);
+}
---
+++ test.ts
@@ -0,0 +1,12 @@
+
+let arregloa: number[] = [1, 2, 3, 4, 5, 6];
+let arreglob: Array<number> = [12, 33, 43, 54, 62];
+
+
+arregloa.forEach(element => {
+    console.log(element);
+});
+
+for (let i = 0; i < arreglob.length; i++) {
+    console.log(arreglob[i]);
+}
